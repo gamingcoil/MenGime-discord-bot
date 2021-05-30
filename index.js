f4a83bae66543c26b901bec2245c104a5eed898b
gamingcoil <gamingcoilyt@gmail.com>
2021-05-30 18:16:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
+
 const prefix = '&';
 
-
-client.on('ready', ()=>{
+client.once('ready', ()=>{
     console.log('This bot is online!');
-})
+});
 
-client.on('message', message=>{
+client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
     const args = message.content.slice(prefix.length).spilt(/ +/);
     const command = args.shift().toLowerCase();
     
-    if(command.content === "helpme"){
-        message.channel.send('צריך אתכם<@829024828287221789>')
+    if(command.content === 'helpme'){
+        message.channel.send('צריך אתכם<@829024828287221789>');
     }
 });
 
