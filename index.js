4bceccc7171f0f5f70304f8722ee5d4420f93b78
gamingcoil <84976465+gamingcoil@users.noreply.github.com>
2021-05-30 17:55:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,12 @@ client.on('ready', ()=>{
     console.log('This bot is online!');
 })
 
-<<<<<<< HEAD
 client.on('message', message=>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).spilt(/ +/);
     const command = args.shift().toLowerCase();
     if(message.content === "helpme"){
         message.reply('צריך אתכם<@829024828287221789>');
-=======
-client.on('message', msg=>{
-    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
-    
-    const args = msg.content.slice(prefix.length).spilt(/ +/);
-    const command = args.shift().toLowerCase();
-    
-    if(command.content === "helpme"){
-        msg.channel.send('צריך אתכם<@829024828287221789>');
->>>>>>> dcdd3341debfd273ad9ef8951fc1090df36220d7
-    }
-})
+
 
 client.login(process.env.token);
